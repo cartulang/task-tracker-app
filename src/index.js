6ee8cdc2d7f30f3dc11686fd23d5ddf51e1dcaa3
cartulang <cartulang@gmail.com>
2021-04-28 23:19:21 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={window.location.pathname || ""}>
+      <Route exact path="/" component={App} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
